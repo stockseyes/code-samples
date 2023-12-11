47ace59b024636bf0334342548335b936925d1bf
Tushar Singhal <singhaltushar903@gmail.com>
2023-12-11 17:02:57 +0530

    update to use instrument token instead of trading symbol

--- node/index.ts
+++ node/index.ts
@@ -3,7 +3,7 @@ import {Operator, setTrigger} from "@stockseyes/market-orders";
 const apiKey = "GET YOUR API KEY at www.stockseyes.com";
 
 const sampletTrigger = async () => {
-   await setTrigger(apiKey, "hkjdkjd", Operator.GREATER_THAN_OR_EQUALS_TO,345, "LARSEN", {"extra meta ": "you will receive in callback body"});
+   await setTrigger(apiKey, "hkjdkjd", Operator.GREATER_THAN_OR_EQUALS_TO,345, 12344, {"extra meta ": "you will receive in callback body"});
 }
 
 sampletTrigger()
